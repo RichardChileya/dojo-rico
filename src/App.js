be4144eee04f9b1a2a58bc23b62152dcbeaffcc5
RichardChileya <richardchileyawk@gmail.com>
2022-02-26 00:50:18 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 function App() {
     const title = 'Geolex Creations Copperation';
     const Random = Math.random() * 20;
-    const link = "https//:aaazambia.org";
+    const link = 'https://www.aaazambia.org';
     return ( < div className = "App" >
 
 
         <div className = "content" >
 
-        < h1 > { title } < /h1>     
+        <h1> { title } </h1>     
         <p > Random Numbers { Random }
         refresh </p>   
         <a href = { link } > AAAZ Site </a>  
