26be367fbc795da7bad71a4fc161c0da0fc035c4
RichardChileya <richardchileyawk@gmail.com>
2022-05-25 12:23:37 +0200

    before 1st branch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import './App.css';
+
 import Navbar from './Navbar';
+import Home from './Home'; 
 
 
 function App() {
     const title = 'Geolex Creations Copperation';
     const Random = Math.random() * 20;
-    const link = 'https://www.aaazambia.org';
-    return ( < div className = "App" >
+    const link = 'https://www.facebook.com/GeolexCreations/';
+    // vid 5
+    return ( < div className = "App" > 
         <Navbar />
         <div className = "content" >
+            <Home />
+            <link/>
+
 
         <h1> { title } </h1>     
         <p > Random Numbers { Random }
         refresh </p>   
-        <a href = { link } > AAAZ Site </a>  
+        <a href = { link } > Visit  </a> 
+         
+        <div>The </div>
+         
         </div>  
         </div>
     );
+
 }
 export default App;
